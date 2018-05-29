39d909d765b82f50bb58c7d3c7c21c158ae4a04d
Shane Vitarana <shanev@gmail.com>
2018-05-28 18:06:34 -0700

    contracts: add emit to events

--- contracts/ERC20UTXO.sol
+++ contracts/ERC20UTXO.sol
@@ -32,7 +32,7 @@ contract ERC20UTXO {
         UTXO memory utxo = UTXO(_to, _value, bytes32(0), id);
         utxos[id] = utxo;
         totalSupply += _value;
-        LogCreate(_to, id, _value);
+        emit LogCreate(_to, id, _value);
     }
 
     function spend(bytes32 _id, uint _amount, address _to) public {
@@ -45,14 +45,14 @@ contract ERC20UTXO {
         bytes32 newId1 = getId(_to, _id);
         UTXO memory spend1 = UTXO(_to, _amount, _id, newId1);
         utxos[newId1] = spend1;
-        LogSpend(msg.sender, _to, oldUtxo.id, newId1, _amount);
+        emit LogSpend(msg.sender, _to, oldUtxo.id, newId1, _amount);
         
         if (_amount < oldUtxo.value) {
             // slightly mutate the _id value to prevent a collision
             bytes32 newId2 = getId(msg.sender, _id ^ bytes32(1)); 
             UTXO memory spend2 = UTXO(msg.sender, oldUtxo.value - _amount, _id, newId2); 
             utxos[newId2] = spend2; 
-            LogSpend(msg.sender, _to, oldUtxo.id, newId2, oldUtxo.value - _amount);
+            emit LogSpend(msg.sender, _to, oldUtxo.id, newId2, oldUtxo.value - _amount);
           }
     }
 }
