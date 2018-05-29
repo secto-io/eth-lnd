2f006fd7e5620c21dbae0fae8db808628d948109
Shane Vitarana <shanev@gmail.com>
2018-05-28 17:54:57 -0700

    contracts: test deploying contract

--- contracts/ERC20UTXO_test.go
+++ contracts/ERC20UTXO_test.go
@@ -1,9 +1,25 @@
 package contracts
 
 import (
+	"math/big"
 	"testing"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	key, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+	addr   = crypto.PubkeyToAddress(key.PublicKey)
 )
 
 func TestERC20UTXO(t *testing.T) {
-
+	contractBackend := backends.NewSimulatedBackend(core.GenesisAlloc{addr: {Balance: big.NewInt(1000000000)}})
+	transactOpts := bind.NewKeyedTransactor(key)
+	_, _, _, err := DeployERC20UTXO(transactOpts, contractBackend)
+	if err != nil {
+		t.Fatalf("can't deploy ERC20UTXO contract: %v", err)
+	}
 }
